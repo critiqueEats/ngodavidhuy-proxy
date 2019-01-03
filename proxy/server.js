6da9967c1f8c7eccf1e4655ef4a5f8ec09191b45
David Ngo <ngo.david.huy@gmail.com>
2019-01-03 11:12:35 -0800

    Updates proxy after pulling from sub modules H

--- proxy/server.js
+++ proxy/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const morgan = require('morgan');
 const path = require('path');
-const app = express();
 const template = require('./template.js');
 const port = process.env.PORT || 1337;
+
+const app = express();
+
+app.use(cors());
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
